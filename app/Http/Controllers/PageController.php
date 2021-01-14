c13068fd0001ae2e845040b3372eaadaf28956b2
Thach Lai <1859044@itec.hcmus.edu.vn>
2021-01-14 18:35:47 +0700

    fix

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -8,8 +8,6 @@ use Illuminate\Support\Facades\DB;
 class PageController extends Controller
 {
     public function home(Request $rq){
-        $user = DB::select('select * from users');
-        var_dump($user);
         return view('page/home');
     }
 }
