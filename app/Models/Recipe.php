fbc11b8b06b36e583cd131834e0796cfa54223f1
Thach Lai <1859044@itec.hcmus.edu.vn>
2021-02-05 01:02:58 +0700

    fix json error

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -7,5 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Recipe extends Model
 {
-
+    // protected $casts = [
+    //     'ingridients' => 'array'
+    // ];
 }
--- resources/views/recipe/show.blade.php
+++ resources/views/recipe/show.blade.php
@@ -2,12 +2,8 @@
 <h4>Author</h4>
 <p>{{$author}}</p>
 <h4>Ingridients:</h4>
-<?php
-    $ingridients = json_decode($recipe->ingridients);
-    var_dump($ingridients);
-?>
-{{-- @foreach ($recipe->ingridients as $ingridient)
+@foreach (json_decode($recipe->ingridients) as $ingridient)
     <p>{{$ingridient}}</p>
-@endforeach --}}
+@endforeach
 <h4>Guide</h4>
 <p>{{$recipe->guide}}</p>
