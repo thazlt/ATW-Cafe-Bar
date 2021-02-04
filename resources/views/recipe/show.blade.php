ff4a6c782917dbd3577b5ab23cf930f0c07a019c
Thach Lai <1859044@itec.hcmus.edu.vn>
2021-02-05 00:27:09 +0700

    test show recipe

--- resources/views/recipe/show.blade.php
+++ resources/views/recipe/show.blade.php
@@ -2,7 +2,7 @@
 <h4>Author</h4>
 <p>{{$author}}</p>
 <h4>Ingridients:</h4>
-{{$recipe->ingridients}}
+{{var_dump(json_decode($recipe->ingridients))}}
 {{-- @foreach ($recipe->ingridients as $ingridient)
     <p>{{$ingridient}}</p>
 @endforeach --}}
