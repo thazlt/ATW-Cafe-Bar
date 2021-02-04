cfde0b8d74dcb838a9ec3fa4db5a34427c41d4e5
Thach Lai <1859044@itec.hcmus.edu.vn>
2021-02-05 04:38:37 +0700

    test

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -18,13 +18,8 @@ class RecipeController extends Controller
     public function show($id){
         $recipe = Recipe::findOrFail($id);
         $author = User::findOrFail($recipe->author);
-        $recipe->ingridients = json_decode($recipe->ingridients);
-        var_dump($recipe->ingridients);
-        $array = ["thaz","thaz"];
-        $json = json_encode($array);
-        var_dump($json);
-        $array2 = \json_decode($json);
-        var_dump($array2);
+        $ingridients = json_decode($recipe->ingridients);
+        var_dump($ingridients);
         return view('recipe.show', ['recipe'=>$recipe, 'author'=>$author->name]);
     }
 }
