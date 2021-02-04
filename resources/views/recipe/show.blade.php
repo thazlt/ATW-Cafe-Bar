0f9f35a24bdf0be5b2b622c5b93f7cdf806e9092
Thach Lai <1859044@itec.hcmus.edu.vn>
2021-02-04 23:58:17 +0700

    test show recipe

--- resources/views/recipe/show.blade.php
+++ resources/views/recipe/show.blade.php
@@ -2,8 +2,9 @@
 <h4>Author</h4>
 <p>{{$author}}</p>
 <h4>Ingridients:</h4>
-@foreach ($recipe->ingridients as $ingridient)
+{{var_dump($recipe)}}
+{{-- @foreach ($recipe->ingridients as $ingridient)
     <p>{{$ingridient}}</p>
-@endforeach
+@endforeach --}}
 <h4>Guide</h4>
 <p>{{$recipe->guide}}</p>
